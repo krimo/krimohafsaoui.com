827d6867a31556198ef90a4cfc5aa475432bef3f
krimo <krimo@kh.com>
2012-07-05 11:49:02 +0200

    Updated styles and js to get better looking font on OSX

--- wp-content/themes/krimohafsaoui/js/script.js
+++ wp-content/themes/krimohafsaoui/js/script.js
@@ -3,6 +3,12 @@
 */
 $(document).ready(function () {
 	$(".global-header-container").addClass('scene');
+	
+	var OSName="";
+	if (navigator.appVersion.indexOf("Mac")!=-1 || navigator.appVersion.indexOf("X11")!=-1 || navigator.appVersion.indexOf("Linux")!=-1) {
+		OSName="macintosh";
+		$('body').addClass(OSName);
+	}
 });
 
 
