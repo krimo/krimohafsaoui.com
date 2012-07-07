2756e491e3232dda4f55f374af8af85aa8f989be
krimo <krimo@kh.com>
2012-07-07 16:28:29 +0200

    display archive-portfolio as front page

--- wp-content/themes/krimohafsaoui/functions.php
+++ wp-content/themes/krimohafsaoui/functions.php
@@ -60,21 +60,6 @@ function create_post_type() {
 	);
 }
 
-add_action("pre_get_posts", "custom_front_page");
-function custom_front_page($wp_query){
-
-    if($wp_query->get('page_id') == get_option('page_on_front')):
-
-        $wp_query->set('post_type', 'portfolio');
-        $wp_query->set('page_id', ''); //Empty
-
-        //Fix conditional Fucntions like is_front_page or is_single ect
-        $wp_query->is_page = 0;
-        $wp_query->is_singular = 0;
-
-    endif;
-
-}
 ?>
 <?php
     function format_comment($comment, $args, $depth) {
