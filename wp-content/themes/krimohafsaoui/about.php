bb3037300c4798b0552e6275211c90ad161dd926
krimo <krimo@kh.com>
2012-07-07 16:09:08 +0200

    dynamic content on about and contact pages

--- wp-content/themes/krimohafsaoui/about.php
+++ wp-content/themes/krimohafsaoui/about.php
@@ -9,14 +9,6 @@ Template Name: About
 </aside>
 
 <article class="main-content">
-	<section>
-		<h2>Who are you?</h2>
-		<p>A web designer &amp; developer based in France. I like to think that I take a no-bullshit approach in how I handle projects as well as clients. I prefer working with decent, reasonable and intelligent people, preferably from all walks of life. I'm uncompromising when it comes to choosing the right type of client and ignoring or firing <a href="http://clientsfromhell.net/">the others</a>. It might sound rough, but it guarantees a solid business relationship while nipping in the bud the horrible ones. As you may thus imagine, I tend to tell things as they are, whether good or bad.</p>
-		<p>Regarding my ideal clients, I'd much rather deal with small business owners and individuals than big corporations. While a huge project/challenge is always fun to take on, I prefer getting to know the person and their business and crafting them something special; as opposed to building bulky soulless products.</p>				
-	</section>
-	<section>
-		<h2>What are your skills?</h2>
-		<p>I'm skilled in PHP, JavaScript, HTML, CSS, Photoshop, Illustrator, InDesign&hellip; But you probably don't care much for all of that technical stuff. So I'm skilled in coming up with solutions to your problems on the web. I'm skilled in establishing long-lasting business relationships.</p>
-	</section>
+	<?php the_content(); ?>
 </article><!-- .main-content --> 
 <?php get_footer(); ?>
--- wp-content/themes/krimohafsaoui/contact.php
+++ wp-content/themes/krimohafsaoui/contact.php
@@ -9,39 +9,6 @@ Template Name: Contact
 </aside>
 
 <article class="main-content">
-	<section>
-		<h2>I will not respond to you if&hellip;</h2>
-		<ul class="no-reply-if">
-			<li>You enquire about free work.</li>
-			<li>You write your email like a teenage girl would write a text message.</li>
-			<li>You can't wait more that a minute to get a reply.</li>
-			<li>You're an idiot.</li>
-		</ul>
-		</section>
-		<section>
-			<h2>On critiques&hellip;</h2>
-			<p>Grammar nazis are welcome. This website being a one-man operation, mistakes will surely be made despite proofreading as much as I can (thanks, tunnel vision). You're also more than welcome to criticize the design or functionality of these pages. If you do so intelligently and respectfully, that is.</p>
-		</section>
-		<section>
-			<h2>Should we work together&hellip;</h2>
-			<p><strong>The client is not king</strong>. As long as you understand that I do not work <em>for</em> you but <em>with</em> you, we'll be good. You're about as responsible as I am for the success of your project, and I will not hesitate to call you out on your <a href="http://www.makemylogobiggercream.com/">bad decisions</a>. I expect the same from you. You know your craft, I know mine, let's respect each other.</p>
-		</section>
-	<section>
-		<h2>Fill out this form, I'll answer in 48h tops</h2>
-		<form action="" class="kh-form">
-			<fieldset>
-				<label for="contact_form_name">Name:</label>
-				<input type="text" required/>
-
-				<label for="contact_form_email">Email:</label>
-				<input type="email" required/>
-
-				<label for="contact_form_body">Your message:</label>
-				<textarea name="contact_form_body" id="" cols="30" rows="10" required></textarea>
-
-				<button type="submit">Send &raquo;</button>
-			</fieldset>
-		</form>
-	</section>
+	<?php the_content(); ?>
 </article><!-- .main-content -->
 <?php get_footer(); ?>
