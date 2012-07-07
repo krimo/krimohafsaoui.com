5b150c2ae1270de3bb77ee862ece864f12ec6c43
krimo <krimo@kh.com>
2012-07-07 15:01:55 +0200

    added prettyprint for code snippets

--- wp-content/themes/krimohafsaoui/js/script.js
+++ wp-content/themes/krimohafsaoui/js/script.js
@@ -1,6 +1,11 @@
 /* Author: Krimo Hafsaoui
 
 */
+
+
+	if ($('body').hasClass('kh-pretty')) {
+		addEventListener('load', function (event) { prettyPrint() }, false);
+	}
 $(document).ready(function () {
 	$(".global-header-container").addClass('scene');
 	
@@ -8,10 +13,6 @@ $(document).ready(function () {
 	if (navigator.appVersion.indexOf("Mac")!=-1 || navigator.appVersion.indexOf("X11")!=-1 || navigator.appVersion.indexOf("Linux")!=-1) {
 		OSName="macintosh";
 		$('body').addClass(OSName);
-	}
-
-	if ($('body').hasClass('kh-pretty')) {
-		addEventListener('load', function (event) { prettyPrint() }, false);
 	}
 });
 
