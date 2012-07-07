2206cfdc7ca6379bce46241b0be3ace4fa8023ef
krimo <krimo@kh.com>
2012-07-07 15:14:57 +0200

    added edit post link on single page

--- wp-content/themes/krimohafsaoui/single.php
+++ wp-content/themes/krimohafsaoui/single.php
@@ -3,6 +3,7 @@
 			<aside class="sidebar single-post-info">
 				<time datetime="<?php the_time('Y-m-d') ?>" pubdate><?php the_time('F jS, Y') ?> <em>by</em> <a href="https://plus.google.com/112687541384446345786" rel="publisher">Krimo</a></time>
 				<a href="#commentssection" class="comments-link"><?php comments_number("No comments yet.", "1 comment &raquo;", "% comments &raquo;"); ?></a>
+				<?php edit_post_link(); ?>
 			</aside>
 			
 			<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
