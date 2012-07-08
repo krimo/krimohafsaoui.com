e0150e05d91c1de25f31fdf6b3ec7a543f00af71
krimo <krimo@kh.com>
2012-07-08 20:01:00 +0200

    still lazy loading

--- wp-content/themes/krimohafsaoui/js/script.js
+++ wp-content/themes/krimohafsaoui/js/script.js
@@ -1,210 +1,36 @@
 /* Author: Krimo Hafsaoui
 
 */
-function elementInViewport(el) {
-  var top = el.offsetTop;
-  var left = el.offsetLeft;
-  var width = el.offsetWidth;
-  var height = el.offsetHeight;
-
-  while(el.offsetParent) {
-    el = el.offsetParent;
-    top += el.offsetTop;
-    left += el.offsetLeft;
-  }
-
-  return (
-    top < (window.pageYOffset + window.innerHeight) &&
-    left < (window.pageXOffset + window.innerWidth) &&
-    (top + height) > window.pageYOffset &&
-    (left + width) > window.pageXOffset
-  );
-}
-
-;(function($){
-  $.extend($.fn, {
-    lazyload: function(){
-      // `this` refers to the current Zepto collection.
-      // When possible, return the Zepto collection to allow chaining.
-          $window = $(window);
-
-    $.fn.lazyload = function(options) {
-        var elements = this;
-        var settings = {
-            threshold       : 0,
-            failure_limit   : 0,
-            event           : "scroll",
-            effect          : "show",
-            container       : window,
-            data_attribute  : "original",
-            skip_invisible  : true,
-            appear          : null,
-            load            : null
-        };
-
-        function update() {
-            var counter = 0;
-      
-            elements.each(function() {
-                var $this = $(this);
-                if (settings.skip_invisible && !elementInViewport($this)) {
-                    return;
-                }
-                if ($.abovethetop(this, settings) ||
-                    $.leftofbegin(this, settings)) {
-                        /* Nothing. */
-                } else if (!$.belowthefold(this, settings) &&
-                    !$.rightoffold(this, settings)) {
-                        $this.trigger("appear");
-                } else {
-                    if (++counter > settings.failure_limit) {
-                        return false;
-                    }
-                }
-            });
-
-        }
-
-        if(options) {
-            /* Maintain BC for a couple of versions. */
-            if (undefined !== options.failurelimit) {
-                options.failure_limit = options.failurelimit; 
-                delete options.failurelimit;
-            }
-            if (undefined !== options.effectspeed) {
-                options.effect_speed = options.effectspeed; 
-                delete options.effectspeed;
-            }
-
-            $.extend(settings, options);
-        }
-
-        /* Cache container as jQuery as object. */
-        $container = (settings.container === undefined ||
-                      settings.container === window) ? $window : $(settings.container);
-
-        /* Fire one scroll event per scroll. Not one scroll event per image. */
-        if (0 === settings.event.indexOf("scroll")) {
-            $container.bind(settings.event, function(event) {
-                return update();
-            });
-        }
-
-        this.each(function() {
-            var self = this;
-            var $self = $(self);
-
-            self.loaded = false;
-
-            /* When appear is triggered load original image. */
-            $self.one("appear", function() {
-                if (!this.loaded) {
-                    if (settings.appear) {
-                        var elements_left = elements.length;
-                        settings.appear.call(self, elements_left, settings);
-                    }
-                    $("<img />")
-                        .bind("load", function() {
-                            $self
-                                .hide()
-                                .attr("src", $self.data(settings.data_attribute))
-                                [settings.effect](settings.effect_speed);
-                            self.loaded = true;
-
-                            /* Remove image from array so it is not looped next time. */
-                            var temp = $.grep(elements, function(element) {
-                                return !element.loaded;
-                            });
-                            elements = $(temp);
-
-                            if (settings.load) {
-                                var elements_left = elements.length;
-                                settings.load.call(self, elements_left, settings);
-                            }
-                        })
-                        .attr("src", $self.data(settings.data_attribute));
-                }
-            });
-
-            /* When wanted event is triggered load original image */
-            /* by triggering appear.                              */
-            if (0 !== settings.event.indexOf("scroll")) {
-                $self.bind(settings.event, function(event) {
-                    if (!self.loaded) {
-                        $self.trigger("appear");
-                    }
-                });
-            }
+(function( $ ) {
+    $.fn.unveil = function () {
+        var images = this, loaded, inview;
+        
+        this.one("unveil", function(){
+            this.setAttribute( "src", this.getAttribute( "data-original" ) );
+            this.removeAttribute( "data-src" );
         });
-
-        /* Check if something appears when window is resized. */
-        $window.bind("resize", function(event) {
-            update();
-        });
-
-        /* Force initial check if images should appear. */
-        update();
+        
+        function unveil () {
+            inview = images.filter(function(){
+                var $e = $(this),
+                    $w = $(window),
+                    wt = $w.scrollTop(),
+                    wb = wt + $w.height(),
+                    et = $e.offset().top,
+                    eb = et + $e.height();
+                    
+                return eb >= wt && et <= wb;
+            });
+            
+            loaded = inview.trigger("unveil");
+            images = images.not( loaded );
+        }
+        
+        $(window).scroll(unveil);
+        unveil();
         
         return this;
     };
-
-    /* Convenience methods in jQuery namespace.           */
-    /* Use as  $.belowthefold(element, {threshold : 100, container : window}) */
-
-    $.belowthefold = function(element, settings) {
-        var fold;
-        
-        if (settings.container === undefined || settings.container === window) {
-            fold = $window.height() + $window.scrollTop();
-        } else {
-            fold = $container.offset().top + $container.height();
-        }
-
-        return fold <= $(element).offset().top - settings.threshold;
-    };
-    
-    $.rightoffold = function(element, settings) {
-        var fold;
-
-        if (settings.container === undefined || settings.container === window) {
-            fold = $window.width() + $window.scrollLeft();
-        } else {
-            fold = $container.offset().left + $container.width();
-        }
-
-        return fold <= $(element).offset().left - settings.threshold;
-    };
-        
-    $.abovethetop = function(element, settings) {
-        var fold;
-        
-        if (settings.container === undefined || settings.container === window) {
-            fold = $window.scrollTop();
-        } else {
-            fold = $container.offset().top;
-        }
-
-        return fold >= $(element).offset().top + settings.threshold  + $(element).height();
-    };
-    
-    $.leftofbegin = function(element, settings) {
-        var fold;
-        
-        if (settings.container === undefined || settings.container === window) {
-            fold = $window.scrollLeft();
-        } else {
-            fold = $container.offset().left;
-        }
-
-        return fold >= $(element).offset().left + settings.threshold + $(element).width();
-    };
-
-    $.inviewport = function(element, settings) {
-         return !$.rightofscreen(element, settings) && !$.leftofscreen(element, settings) && 
-                !$.belowthefold(element, settings) && !$.abovethetop(element, settings);
-    }
-   }
-  });
 })(Zepto);
 
 $(document).ready(function () {
@@ -218,8 +44,10 @@ $(document).ready(function () {
 
 	if ($('body').hasClass('kh-pretty')) {prettyPrint();}
 
-	$("img").lazyload({ 
-    	effect : "fadeIn"
+	$("img").each(function() {
+		if ($this.attr('data-original')) {
+			$this.unveil();
+		}
 	});
 });
 
