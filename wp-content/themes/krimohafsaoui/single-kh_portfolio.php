5699eb52b0800f8d6ff7ce31f040436daead3567
krimo <krimo@kh.com>
2012-07-08 16:19:35 +0200

    single portfolio building

--- wp-content/themes/krimohafsaoui/single-kh_portfolio.php
+++ wp-content/themes/krimohafsaoui/single-kh_portfolio.php
@@ -7,7 +7,7 @@
 					<li><strong>Client:</strong> <?=get_post_meta($post->ID, 'kh_project_client', true); ?></li>
 					<li><strong>Date:</strong> <?=get_post_meta($post->ID, 'kh_project_date', true); ?></li>
 					<?php if (get_post_meta($post->ID, 'kh_project_url', true)) { ?>
-					<li><strong>Project URL:</strong> <a href="<?=get_post_meta($post->ID, 'kh_project_url', true); ?>">visit &raquo;</a></li>
+					<li><strong>Project URL:</strong> <a href="<?=get_post_meta($post->ID, 'kh_project_url', true); ?>" target="_blank">visit &raquo;</a></li>
 					<?php } ?>
 				</ul>
 			</aside>
