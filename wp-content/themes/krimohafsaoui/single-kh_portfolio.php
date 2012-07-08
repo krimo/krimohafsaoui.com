03b55010c8412bf53eedd2acbc3dff5f2b74e1e1
krimo <krimo@kh.com>
2012-07-08 13:51:22 +0200

    single portfolio piece

--- wp-content/themes/krimohafsaoui/single-kh_portfolio.php
+++ wp-content/themes/krimohafsaoui/single-kh_portfolio.php
@@ -1,12 +1,15 @@
 <?php get_header(); ?>
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<aside class="sidebar single-post-info">
-				<time datetime="<?php the_time('Y-m-d') ?>" pubdate><?php the_time('F jS, Y') ?> <em>by</em> <a href="https://plus.google.com/112687541384446345786" rel="publisher">Krimo</a></time>
-				<a href="#commentssection" class="comments-link"><?php comments_number("Leave a comment &raquo;", "1 comment &raquo;", "% comments &raquo;"); ?></a>
 				<?php edit_post_link(); ?>
+				<ul>
+					<li>Project type: <?php get_post_meta($post->ID, 'kh_project_type', true); ?></li>
+					<li>Client: <?php get_post_meta($post->ID, 'kh_project_client', true); ?></li>
+					<li>Date: <?php get_post_meta($post->ID, 'kh_project_date', true); ?></li>
+				</ul>
 			</aside>
 			
-			<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
+			<article <?php post_class('main-content single-portfolio-main-content') ?> id="post-<?php the_ID(); ?>">
 				<?php the_content(); ?>
 			<?php endwhile; endif; ?>
 			<footer class="clearfix single-post-footer">
