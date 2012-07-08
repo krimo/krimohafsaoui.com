a8939dbf57d7457ee3e313d29f42070bfb3f2de7
krimo <krimo@kh.com>
2012-07-08 08:42:09 +0200

    changed about background

--- wp-content/themes/krimohafsaoui/footer.php
+++ wp-content/themes/krimohafsaoui/footer.php
@@ -4,7 +4,7 @@
 	<div class="global-footer-container">
 		<footer class="wrapper clearfix">
 			<small><strong>&copy; Krimo Hafsaoui 2012</strong>. This site uses HTML5, CSS3, Zepto.js and Wordpress.</small>
-			<div class="global-footer-links"><a href="https://plus.google.com/112687541384446345786" rel="publisher" class="global-footer-link">Google+</a> | <a href="http://www.linkedin.com/profile/view?id=60378454" class="global-footer-link">LinkedIn</a> | <a href="#" class="global-footer-link">Facebook</a></div>
+			<div class="global-footer-links"><a href="https://plus.google.com/112687541384446345786" rel="publisher" class="global-footer-link">Google+</a> | <a href="http://www.linkedin.com/in/krimohafsaoui" class="global-footer-link">LinkedIn</a> | <a href="#" class="global-footer-link">Facebook</a></div>
 		</footer>
 	</div>
 
