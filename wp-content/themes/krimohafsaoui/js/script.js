4e88b696d34f861e4d6a64274e13a05a6db019af
krimo <krimo@kh.com>
2012-07-09 10:29:19 +0200

    still lazy loading

--- wp-content/themes/krimohafsaoui/js/script.js
+++ wp-content/themes/krimohafsaoui/js/script.js
@@ -1,38 +1,6 @@
 /* Author: Krimo Hafsaoui
 
 */
-;(function($){
-  $.extend($.fn, {
-    unveil: function(){
-        var images = this, loaded, inview;
-        
-        this.one("unveil", function(){
-            this.setAttribute( "src", this.getAttribute( "data-original" ) );
-            this.removeAttribute( "data-original" );
-        });
-        
-        function unveil () {
-            inview = images.filter(function(){
-                var $e = $(this),
-                    $w = $(window),
-                    wt = $w.scrollTop(),
-                    wb = wt + $w.height(),
-                    et = $e.offset().top,
-                    eb = et + $e.height();
-                    
-                return eb >= wt && et <= wb;
-            });
-            
-            loaded = inview.trigger("unveil");
-            images = images.not( loaded );
-        }
-        
-        $(window).scroll(unveil);
-        unveil();
-      return this;
-    }
-  })
-})(Zepto)
 
 $(document).ready(function () {
 	$(".global-header-container").addClass('scene');
@@ -45,7 +13,6 @@ $(document).ready(function () {
 
 	if ($('body').hasClass('kh-pretty')) {prettyPrint();}
 
-	$("img").unveil();
 });
 
 
