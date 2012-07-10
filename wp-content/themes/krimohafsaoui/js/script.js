75819b5d9bd2dd81a5d878be018a34a0774765f5
krimo <krimo@kh.com>
2012-07-10 20:28:52 +0200

    changed script

--- wp-content/themes/krimohafsaoui/js/script.js
+++ wp-content/themes/krimohafsaoui/js/script.js
@@ -3,8 +3,20 @@
 */
 
 $(document).ready(function () {
-	
-	$(".global-header-container").addClass('scene');
+	(function () {
+		var img = new Image(), srcs = ['Blog', 'Work', 'About', 'Contact'], bgs = ['blog-bg.png', 'work-bg.png', 'about-bg.png', 'contact-bg.png'];
+		
+		for (i=0;i<4;i++) {
+			if ($('body').hasClass(srcs[i])) {
+				theSource = 'http://www.krimohafsaoui.com/wp-content/themes/krimohafsaoui/img/'+bgs[i];
+				img.src = theSource;
+				img.onLoad = function () { /* onLoad will only fire once and not if the image is cached, FUCK! */
+					$('.animated-header-bg').css('background-image', theSource);
+					$(".global-header-container").addClass('scene');
+				}
+			}
+		};
+	}); /* Function NOT YET PUSHED LIVE to smooth header animation.*/
 	
 	var OSName="";
 	if (navigator.appVersion.indexOf("Mac")!=-1 || navigator.appVersion.indexOf("X11")!=-1 || navigator.appVersion.indexOf("Linux")!=-1) {
