55ba908747e968997c977c72888e0d8539a27b46
krimo <krimo@kh.com>
2012-07-10 20:46:45 +0200

    scripting

--- wp-content/themes/krimohafsaoui/js/script.js
+++ wp-content/themes/krimohafsaoui/js/script.js
@@ -2,23 +2,11 @@
 
 */
 
+$(window).load = function() {
+	$('.global-header-container').addClass('scene');
+}
+
 $(document).ready(function () {
-	var robert = function () {
-		var img = new Image(), srcs = ['Blog', 'Work', 'About', 'Contact'], bgs = ['blog-bg.png', 'work-bg.png', 'about-bg.png', 'contact-bg.png'];
-		
-		for (i=0;i<4;i++) {
-			if ($('body').hasClass(srcs[i])) {
-				theSource = 'http://www.krimohafsaoui.com/wp-content/themes/krimohafsaoui/img/'+bgs[i];
-				$('body').load(theSource, function () {
-					$('.animated-header-bg').css('background-image', 'url('+theSource+')');
-					$(".global-header-container").addClass('scene');
-				});
-			}
-		};
-	}; /* Function NOT YET PUSHED LIVE to smooth header animation.*/
-	
-	robert();
-	
 	var OSName="";
 	if (navigator.appVersion.indexOf("Mac")!=-1 || navigator.appVersion.indexOf("X11")!=-1 || navigator.appVersion.indexOf("Linux")!=-1) {
 		OSName="macintosh";
@@ -26,6 +14,8 @@ $(document).ready(function () {
 	}
 
 	if ($('body').hasClass('kh-pretty')) {prettyPrint();}
+	
+	
 
 });
 
