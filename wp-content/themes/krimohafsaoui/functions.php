ac694389adca1cc4142236b92fa3621526769a65
krimo <krimo@kh.com>
2012-07-13 14:15:27 +0200

    working on inserting image tag with custom html

--- wp-content/themes/krimohafsaoui/functions.php
+++ wp-content/themes/krimohafsaoui/functions.php
@@ -62,7 +62,7 @@ function create_post_type() {
 }
 
 add_filter('get_image_tag', 'kh_image_attachment');
-function kh_image_attachment( $id, $alt, $title, $align)
+function kh_image_attachment( $html, $id, $alt, $title, $align, 10, 5)
 {
 	$html = '<img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="' . esc_attr($img_src) . '" alt="' . esc_attr($alt) . '" title="' . esc_attr($title).'" class="'.$class.'" onload=lzld(this) onerror=lzld(this) />';
 	return $html;
