e97898828b0d705712b488e15f57a173b04c1d49
krimo <krimo@kh.com>
2012-07-13 13:54:01 +0200

    styling contact form

--- wp-content/themes/krimohafsaoui/functions.php
+++ wp-content/themes/krimohafsaoui/functions.php
@@ -62,16 +62,9 @@ function create_post_type() {
 }
 
 add_filter('get_image_tag', 'kh_image_attachment');
-function kh_image_attachment($html,$id, $alt, $title)
+function kh_image_attachment( $id, $alt, $title, $align)
 {
-
-	list( $img_src, $width, $height ) = image_downsize($id, $size);
-	$hwstring = image_hwstring($width, $height);
-
-	$class = 'align' . esc_attr($align) .' size-' . esc_attr($size) . ' wp-image-' . $id;
-	$class = apply_filters('get_image_tag_class', $class, $id, $align, $size);
-	
-	$html = '<img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="' . esc_attr($img_src) . '" alt="' . esc_attr($alt) . '" title="' . esc_attr($title).'" '.$hwstring.'class="'.$class.'" onload=lzld(this) onerror=lzld(this) />';;
+	$html = '<img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="' . esc_attr($img_src) . '" alt="' . esc_attr($alt) . '" title="' . esc_attr($title).'" class="'.$class.'" onload=lzld(this) onerror=lzld(this) />';
 	return $html;
 }
 
