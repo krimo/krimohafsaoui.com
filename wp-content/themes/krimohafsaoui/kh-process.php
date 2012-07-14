ecfb806ba3a3262d7d71de6e58b1a56d6a8ae341
krimo <krimo@kh.com>
2012-07-14 15:52:41 +0200

    launch

--- wp-content/themes/krimohafsaoui/kh-process.php
+++ wp-content/themes/krimohafsaoui/kh-process.php
@@ -1,3 +1,13 @@
 <?php
-echo "success";
+if (isset($_POST)) {
+	$name = filter_var($_POST['contact_form_name'], FILTER_SANITIZE_STRING);
+	$email = filter_var($_POST['contact_form_email'], FILTER_SANITIZE_EMAIL);
+	$message = filter_var($_POST['contact_form_body'], FILTER_SANITIZE_STRING);
+
+	$to      = 'krimo@krimohafsaoui.com';
+	$subject = 'A new message from '.$name;
+	$headers = 'From: '. $email . "\r\n";
+
+	mail($to, $subject, $message, $headers);
+}
 ?>
