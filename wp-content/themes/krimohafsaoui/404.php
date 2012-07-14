6f0ab4caad2ead16147bfc2fe3845282575b270b
krimo <krimo@kh.com>
2012-07-14 16:52:04 +0200

    testing mail on personal address

--- wp-content/themes/krimohafsaoui/404.php
+++ wp-content/themes/krimohafsaoui/404.php
@@ -8,5 +8,4 @@
 		<p>Ok so it looks like you tried to access something that A) didn't exist, B) existed in the past but doesn't anymore. So you should respectively <a href="/">go back to the homepage</a> and try to find what you were looking from there, or if you feel like it, <a href="/contact">shoot me an email</a> and tell me what you were trying to access, it'd be much appreciated.</p>				
 	</section>
 </article><!-- .main-content -->
-<?php } ?>
 <?php get_footer(); ?>
